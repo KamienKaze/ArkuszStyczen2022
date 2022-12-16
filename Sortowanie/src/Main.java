495f5ab1adfacf0836a0e58f275eb0efab654c80
KamienKaze <HygrekRazuuk@gmail.com>
2022-12-16 13:13:20 +0100

    sortowanie

--- Sortowanie/src/Main.java
+++ Sortowanie/src/Main.java
@@ -1,11 +1,27 @@
-
 public class Main {
-
-    int[] array;
-
-    public static void main(String[] args) {
-
-        Main main = new Main();
-        main.array = new int[10];
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[index]){
+                    index = j;
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
     }
-}
+
+    public static void main(String a[]) {
+
+        int[] arr1 = {6, 32, 7, 64, 33, 11, 18, 54};
+
+        selectionSort(arr1);
+
+        for(int i:arr1){
+            System.out.print(i + " ");
+        }
+    }
+}  
